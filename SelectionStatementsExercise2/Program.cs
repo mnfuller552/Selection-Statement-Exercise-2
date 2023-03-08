f9827339027a7c49a88b33991eb6b4130b337a88
Matt Fuller <mnfuller552@gmail.com>
2023-03-08 09:09:20 -0500

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"What's your favorite subject in school?");
+
+            string userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+                case "math":
+                    Console.WriteLine($"You must be pretty smart!");
+                    break;
+                case "science":
+                    Console.WriteLine($"Science is really interesting!");
+                    break;
+                case "english":
+                    Console.WriteLine($"CooL! Definitely my worst subject in school!");
+                    break;
+                case "history":
+                    Console.WriteLine($"I hated history!");
+                    break;
+                case "music":
+                    Console.WriteLine($"My favorite! Do you play any instruments?");
+                    break;
+
+                default:
+                    Console.WriteLine($"Nice! I've never taken that before.");
+                    break;
+
+
+
+
+
+
+            }
         }
     }
 }
